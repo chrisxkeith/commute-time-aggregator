5be6748d8d6c32835ea13d344648aa5ef1599dd2
Chris <chris.keith@gmail.com>
2013-04-19 05:45:32 -0700

    Another attempt to fill in missing data slots. Formatting and comment cleanup.

--- DurationCollector/src/me/chriskeith/dc/DurationCollector2.java
+++ DurationCollector/src/me/chriskeith/dc/DurationCollector2.java
@@ -20,33 +20,35 @@ import java.text.SimpleDateFormat;
  * @author ckeith
  */
 public class DurationCollector2 {
-	
+
 	class CollectionParams {
 		String personId;
 		String homeLocation;
 		String workLocation;
-		
-		CollectionParams(String personId, String homeLocation, String workLocation) {
+
+		CollectionParams(String personId, String homeLocation,
+				String workLocation) {
 			this.personId = personId;
 			this.homeLocation = homeLocation;
 			this.workLocation = workLocation;
 		}
 	}
-	
-	private List<CollectionParams> collectionParams;
+
 	final private String dirForResults = "/tmp";
 	final private Pattern digitPattern = Pattern.compile("[0-9]+");
 	final private String otherCollectionParamsFileName;
-	
+
 	// Sample every two minutes.
 	final private int minuteInterval = 2;
-	
-	// A format that will convert to a date when pasted into a Google spreadsheet.
-	final private SimpleDateFormat outputDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 
-	final private SimpleDateFormat debuggingDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	// A format that will convert to a date when pasted into a Google
+	// spreadsheet.
+	final private SimpleDateFormat outputDateFormat = new SimpleDateFormat(
+			"yyyy/MM/dd HH:mm");
+	final private List<CollectionParams> collectionParams = new ArrayList<CollectionParams>();
 
 	private WebDriver driver = null;
+	private Calendar previousSlot = null;
 
 	public DurationCollector2(String[] args) {
 		if (args.length > 0) {
@@ -107,9 +109,10 @@ public class DurationCollector2 {
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		}
 	}
-	
-	private BufferedWriter getWrite(String personId) throws Exception {
-		String filePathString = dirForResults + "/" + personId + "_commuteTimes.txt";
+
+	private BufferedWriter getWriter(String personId) throws Exception {
+		String filePathString = dirForResults + "/" + personId
+				+ "_commuteTimes.txt";
 		FileWriter fstream;
 		File f = new File(filePathString);
 		if (f.exists()) {
@@ -121,25 +124,25 @@ public class DurationCollector2 {
 	}
 
 	private void collectDurations() throws Exception {
-			String origin;
-			String destination;
-			while (true) {
-				sleepUntilNextSnapshot();
-				int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-				for (CollectionParams cp : collectionParams) {
-					if (hour < 12) { // switch directions at 12 noon.
-						origin = cp.homeLocation;
-						destination = cp.workLocation;
-					} else {
-						origin = cp.workLocation;
-						destination = cp.homeLocation;
-					}
-					int newDuration = this.collectDuration(origin, destination);
-					writeDuration(cp.personId, new Date(), newDuration);
+		String origin;
+		String destination;
+		while (true) {
+			sleepUntilNextSnapshot();
+			int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+			for (CollectionParams cp : collectionParams) {
+				if (hour < 12) { // switch directions at 12 noon.
+					origin = cp.homeLocation;
+					destination = cp.workLocation;
+				} else {
+					origin = cp.workLocation;
+					destination = cp.homeLocation;
 				}
+				int newDuration = this.collectDuration(origin, destination);
+				writeDuration(cp.personId, newDuration);
 			}
+		}
 	}
-	
+
 	private void sleepUntilNextSnapshot() throws Exception {
 		Calendar now = Calendar.getInstance();
 		int dayOfWeek = now.get(Calendar.DAY_OF_WEEK);
@@ -150,7 +153,7 @@ public class DurationCollector2 {
 			dayIncrement = 1;
 		} else {
 			int hour = now.get(Calendar.HOUR_OF_DAY);
-			// If outside the 8 p.m. to 4 a.m. range, sleep until 4 a.m.
+			// If inside the 8 p.m. to 4 a.m. range, sleep until 4 a.m.
 			if (19 < hour) {
 				if (dayOfWeek == Calendar.FRIDAY) {
 					dayIncrement = 3;
@@ -162,44 +165,118 @@ public class DurationCollector2 {
 				dayIncrement = 1;
 			}
 		}
-		Calendar start = (Calendar)now.clone();
+		Calendar start = (Calendar) now.clone();
 		if (dayIncrement > 0) {
-			// Assume (till proved otherwise) that this will this work on the last day of the year.
-			start.set(Calendar.DAY_OF_YEAR, start.get(Calendar.DAY_OF_YEAR) + dayIncrement);
+			// Assume (till proved otherwise) that this will this work on the
+			// last day of the year.
+			start.set(Calendar.DAY_OF_YEAR, start.get(Calendar.DAY_OF_YEAR)
+					+ dayIncrement);
 			start.set(Calendar.HOUR_OF_DAY, 4);
 			start.set(Calendar.MINUTE, 0);
-			
-			// Reload in case we've manually edited the file containing the list of routes.
+
+			// Reload in case we've manually edited the file containing the list
+			// of routes.
 			loadCollectionParams();
 		} else {
 			// Round to previous minute instant.
-			int lastInstant = (start.get(Calendar.MINUTE) / minuteInterval) * minuteInterval;
+			int lastInstant = (start.get(Calendar.MINUTE) / minuteInterval)
+					* minuteInterval;
 			// Sync to next minuteInterval instant.
 			start.set(Calendar.MINUTE, (lastInstant + minuteInterval));
 		}
 		start.set(Calendar.SECOND, 0);
 		long millis = start.getTimeInMillis() - now.getTimeInMillis();
-		System.out.println("About to sleep until " + this.debuggingDateFormat.format(new Date(start.getTimeInMillis())));
+		if (dayIncrement > 0) {
+			System.out
+					.println("About to sleep until "
+							+ outputDateFormat.format(new Date(start
+									.getTimeInMillis())));
+		}
 		Thread.sleep(millis);
 	}
-	
-	private void writeDuration(String personId, Date date, int duration) throws Exception {
-		if (0 < duration) {
-			BufferedWriter out = this.getWrite(personId);
-			// If we didn't get a value, write an empty slot to keep slots across different days in sync.
-			String s = outputDateFormat.format(date) + "\t" + ((duration < Integer.MAX_VALUE) ? duration : "");
-			out.write(s + System.getProperty("line.separator"));
-			out.flush();
-			System.out.println(s);
-			out.close();
+
+	private void writeDuration(String personId, int duration) throws Exception {
+		Calendar now = Calendar.getInstance();
+		// Is it possible that "now" will be more than one minute past the slot?
+		now.set(Calendar.SECOND, 0);
+		// If we didn't get a value, write empty slot(s) to keep slots across
+		// different days in sync.
+		String durationStr = "";
+		BufferedWriter out = this.getWriter(personId);
+		if (previousSlot != null) {
+			previousSlot.add(Calendar.MINUTE, this.minuteInterval);
+			while (previousSlot.getTimeInMillis() < now.getTimeInMillis()) {
+				String s = outputDateFormat.format(new Date(now
+						.getTimeInMillis()))
+						+ "\t"
+						+ durationStr
+						+ System.getProperty("line.separator");
+				out.write(s);
+				previousSlot.add(Calendar.MINUTE, this.minuteInterval);
+			}
 		}
+		if ((0 < duration) && (duration < Integer.MAX_VALUE)) {
+			durationStr = new Integer(duration).toString();
+		}
+		String s = outputDateFormat.format(new Date(now.getTimeInMillis()))
+				+ "\t" + durationStr + System.getProperty("line.separator");
+		out.write(s);
+		out.close();
+		previousSlot = now;
 	}
-	
-	private void runWithRetries() {
+
+	private void loadCollectionParams() throws Exception {
+		collectionParams.clear();
+		if (otherCollectionParamsFileName != null) {
+			File otherCollectionParams = new File(otherCollectionParamsFileName);
+			if (!otherCollectionParams.exists()) {
+				throw new RuntimeException("Can't find: "
+						+ otherCollectionParams.getAbsolutePath());
+			}
+			BufferedReader br = new BufferedReader(new FileReader(
+					otherCollectionParamsFileName));
+			try {
+				String personId = br.readLine();
+				while (personId != null) {
+					String home = br.readLine();
+					if (home == null) {
+						throw new RuntimeException("Home not specified for: "
+								+ personId);
+					}
+					String work = br.readLine();
+					if (work == null) {
+						throw new RuntimeException("Work not specified for: "
+								+ personId);
+					}
+					collectionParams.add(new CollectionParams(personId, home,
+							work));
+					personId = br.readLine();
+				}
+			} finally {
+				br.close();
+			}
+		}
+		// Add this one last, so the browser shows it (to help
+		// debugging/monitoring).
+		collectionParams.add(new CollectionParams("ChristopherKeith",
+				"368 MacArthur Blvd, San Leandro, CA 94577",
+				"3200 Bridge Pkwy Redwood City, CA"));
+	}
+
+	public void run() {
+		File dir = new File(dirForResults);
+		if (!dir.exists()) {
+			if (!dir.mkdir()) {
+				throw new RuntimeException("Unable to create: "
+						+ dir.getAbsolutePath());
+			}
+		}
 		try {
+			loadCollectionParams();
 			collectDurations();
 		} catch (Exception e) {
-			System.out.println(new Date().toString() + System.getProperty("line.separator") + e);
+			e.printStackTrace();
+			System.out.println(new Date().toString() + e);
 		} finally {
 			if (driver != null) {
 				driver.quit();
@@ -207,54 +284,7 @@ public class DurationCollector2 {
 		}
 	}
 
-	private void loadCollectionParams() throws Exception {
-		collectionParams = new ArrayList<CollectionParams>(1);
-		if (otherCollectionParamsFileName != null) {
-			File otherCollectionParams = new File(otherCollectionParamsFileName);
-			if (!otherCollectionParams.exists()) {
-				throw new RuntimeException("Can't find: " + otherCollectionParams.getAbsolutePath());
-			}
-			BufferedReader br = new BufferedReader(new FileReader(otherCollectionParamsFileName));
-		    try {
-		        String personId = br.readLine();
-		        while (personId != null) {
-		        	String home = br.readLine();
-		        	if (home == null) {
-						throw new RuntimeException("Home not specified for: " + personId);
-		        	}
-		        	String work = br.readLine();
-		        	if (work == null) {
-						throw new RuntimeException("Work not specified for: " + personId);
-		        	}
-		        	collectionParams.add(new CollectionParams(personId, home, work));
-		        	personId = br.readLine();
-		        }
-		    } finally {
-		        br.close();
-		    }
-		}
-		// Add this one last, so the browser shows it (to help debugging/monitoring).
-		collectionParams.add(new CollectionParams("ChristopherKeith", 
-				"368 MacArthur Blvd, San Leandro, CA 94577",
-				"3200 Bridge Pkwy Redwood City, CA"));
-	}
-	
-	public void run() throws Exception {
-		File dir = new File(dirForResults);
-		if (!dir.exists()) {
-			if (!dir.mkdir()) {
-				throw new RuntimeException("Unable to create: " + dir.getAbsolutePath());
-			}
-		}
-		loadCollectionParams();
-		runWithRetries();
-	}
-
 	public static void main(String[] args) {
-		try {
-			new DurationCollector2(args).run();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+		new DurationCollector2(args).run();
 	}
 }
