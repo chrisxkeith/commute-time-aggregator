bae0c6e07d2e740e72b4a9fc3a4357dceaaf7a1e
CK <chris.keith@gmail.com>
2019-11-26 16:16:07 -0800

    Make it easier to copy cmd line for console window.

--- DurationCollector/scripts/run.sh
+++ DurationCollector/scripts/run.sh
@@ -2,6 +2,5 @@ set -x
 cd ~/Documents/Github/commute-time-aggregator/DurationCollector/	; if [ $? -ne 0 ] ; then exit -6 ; fi
 cat ~/Documents/routeInfo.txt										; if [ $? -ne 0 ] ; then exit -6 ; fi
 
-mvn clean install exec:java -Dexec.mainClass="com.ckkeith.duration_collector.DurationCollector" \
- -Dmaven.test.skip=true | \
+mvn clean install exec:java -Dexec.mainClass="com.ckkeith.duration_collector.DurationCollector" -Dmaven.test.skip=true | \
  tee -a ~/Documents/route-data/routeInfo.log
